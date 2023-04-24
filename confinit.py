330ea6acdabffa1bb95b2233d8d45d5767fb64f6
Victor <sky@tinirog.ru>
2023-04-24 16:47:45 +0300

    bug fix

--- confinit.py
+++ confinit.py
@@ -56,7 +56,7 @@ def getip():
     for i in ifaces:
         if 'eth' in i: config['eth'].append(i)
         if 'wlan' in i: config['wlan'].append(i) 
-        if 'ens' in i: config['enp'].append(i)
+        if 'ens' in i: config['ens'].append(i)
     for key in config:
         config[key].sort()
         if config[key]:
