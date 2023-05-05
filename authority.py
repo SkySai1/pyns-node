8c60975f85808c57c37428d586e89e7bca15c82a
Vitaly <sky@tinirog.ru>
2023-05-05 19:00:13 +0000

    fully changed dnslib on dnspython functions

--- authority.py
+++ authority.py
@@ -2,6 +2,10 @@ from functools import lru_cache
 from dnslib import DNSRecord, RR, QTYPE, CLASS
 from sqlalchemy import create_engine
 from accessdb import AccessDB
+import dns.message
+import dns.rrset
+import dns.flags
+import dns.name
 #from caching import Caching
 
 class Authority:
@@ -12,45 +16,48 @@ class Authority:
 
     def authority(self, packet):
         result, auth, data = Authority.resolve(self, packet)
+        query = dns.message.from_wire(packet)
+        answer = dns.message.make_response(query)
         if result or auth:
-            answer = data.reply()
-            if auth: answer = makeanswer(answer,auth, 0)    
-            if result: answer = makeanswer(answer,result, 2)                                         
-            answer.header.set_aa(1)
+            answer.origin = query.question[0].name
+            if result: answer = makeanswer(answer,result, 0)  
+            elif auth: answer = makeanswer(answer,auth, 1)                                           
+            answer.flags += dns.flags.AA
         else: # <- if server didn't know about qname it will try to resolve it
             answer = data
-            answer.header.set_rcode(3)
-            answer.header.set_qr(1)
-        return answer.pack(), answer
+            answer.set_rcode(3)
+        return answer.to_wire(), answer
 
     def resolve(self, packet):
-        data = DNSRecord.parse(packet)
-        db = AccessDB(self.engine, self.conf)
-        Q = {}
-        Q['name'] = str(data.get_q().qname)
-        Q['class'] = CLASS[data.get_q().qclass]
-        Q['type'] = QTYPE[data.get_q().qtype]
-        auth = None
-        result = db.getDomain(Q['name'], Q['class'], Q['type']) # <- Get RR from Domain Table
-        if not result: # <- if not exists required RR type then return authority list
-            auth = db.getDomain(Q['name'], Q['class'], 'NS') # <- Check Authority
-        if not result and not auth: # <- if not authority list and RR then check in cache
-            result = db.getCache(Q['name'], Q['class'], Q['type']) # <- Get RR from Cache Table
-        return result, auth, data
+        data = dns.message.from_wire(packet)
+        if data.question:
+            rr = data.question[-1].to_text().split(' ')
+            db = AccessDB(self.engine, self.conf)
+            Q = {}
+            Q['name'] = rr[0]
+            Q['class'] = rr[1]
+            Q['type'] = rr[2]
+            auth = None
+            result = db.getDomain(Q['name'], Q['class'], Q['type']) # <- Get RR from Domain Table
+            if not result: # <- if not exists required RR type then return authority list
+                auth = db.getDomain(Q['name'], Q['class'], 'NS') # <- Check Authority
+            if not result and not auth: # <- if not authority list and RR then check in cache
+                result = db.getCache(Q['name'], Q['class'], Q['type']) # <- Get RR from Cache Table
+            return result, auth, data
     
-def makeanswer(answer:DNSRecord, dbresult, type = None):
+def makeanswer(answer:dns.message.Message, dbresult, type = None):
     """
     type 0 - for auth section
     type 1 - for additional section
     """
     for obj in dbresult:
         for row in obj:
-            record = f"{row.name} {str(row.ttl)} {row.dclass} {row.type} {row.data}"
+            record = dns.rrset.from_text(row.name, row.ttl, row.dclass, row.type, row.data)
             if not record: break
             if type == 0:
-                answer.add_auth(*RR.fromZone(record))
+                answer.answer.append(record)
             if type == 1:
-                answer.add_ar(*RR.fromZone(record))
+                answer.additional.append(record)
             elif type == 2:
                 if answer.get_q().qtype == 1 and row.type == 'CNAME': # <- Break answer for getting addr on A question in CNAME record 
                     answer.header.set_rcode(3) 
