e64a709b75ca64fc787c4d1d1d2224ee810ec155
Vitaly <sky@tinirog.ru>
2023-05-07 10:46:57 +0000

    Bug Fix (After making total precaching)

--- caching.py
+++ caching.py
@@ -36,7 +36,7 @@ class Caching:
             if not packet: packet = data.to_wire(data.question[0].name)
             _CACHE[record] = packet
             threading.Thread(target=Caching.clearcache, args=(self, record)).start()
-            print(f'{datetime.datetime.now()}: {data.question[0].to_text()} was cached as {record}')
+            #print(f'{datetime.datetime.now()}: {data.question[0].to_text()} was cached as {record}')
 
     def clearcache(self, record):
         time.sleep(self.conf['buffertime'])
@@ -46,10 +46,10 @@ class Caching:
             packet,_ = Caching.precache(self, name, rdtype, rdclass)
             if packet:
                 _CACHE[record] = packet
-                print(f'{datetime.datetime.now()}: {name, rdclass, rdtype} was PREcached')
+                #print(f'{datetime.datetime.now()}: {name, rdclass, rdtype} was PREcached')
                 Caching.clearcache(self, record)
             else:
-                print(f'{datetime.datetime.now()}: {name, rdclass, rdtype} was uncached')
+                #print(f'{datetime.datetime.now()}: {name, rdclass, rdtype} was uncached')
                 del _CACHE[record]
 
     def precache(self, name, rdtype, rdclass):
